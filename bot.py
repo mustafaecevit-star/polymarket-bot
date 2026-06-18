b3ee57a170b8a7f7619eb3309f81982706412603
mustafaecevit-star <mustafaecevit@yahoo.com>
2026-06-18 14:08:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,13 @@
+import http.server
+import socketserver
+import os
+
 print("Polymarket Botu Basariyla Calisti!")
-while True: pass
+
+# Render'in istedigi sahte web kapisi (port) ayari
+PORT = int(os.environ.get("PORT", 10000))
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Sahte sunucu {PORT} portunda aktif.")
+    httpd.serve_forever()
+
