4424f8baa2771327e401161821159caf8f6871c5
mustafaecevit-star <mustafaecevit@yahoo.com>
2026-06-18 17:12:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
-import os
+ import telebot
+TOKEN = '8969386410:AAG1-VlFwjup rtyuYFJkoup7a0W2NHrGZeg'
+bot = telebot.TeleBot(TOKEN.replace(' ', ''))
+ import os
 import time
 import threading
 import http.server
